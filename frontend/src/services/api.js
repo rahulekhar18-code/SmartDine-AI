1b1a4956f8e7afe2dec8a1cc628ce6e47d459fc9
Rahul Ekhar <rahulekhar18@gmail.com>
2026-06-23 03:44:10 +0530

    css change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`
-});
+  baseURL:"http://localhost:5000/api"});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
